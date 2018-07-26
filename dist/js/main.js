8f7003cb1f8dc86371a4f318bf3489664daf8d08
Manuel <m.a.c.p90@gmail.com>
2018-07-26 16:49:59 -0400

    work, about home and projects created

--- dist/js/main.js
+++ dist/js/main.js
@@ -31,15 +31,6 @@ function toggleMenu() {
     showMenu = false;
   }
 }
-// Select DOM Items
-const menuBtn = document.querySelector(".menu-btn");
-const menu = document.querySelector(".menu");
-const menuNav = document.querySelector(".menu-nav");
-const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
-
-// Set Initial State Of Menu
-let showMenu = false;
 
 menuBtn.addEventListener("click", toggleMenu);
 
